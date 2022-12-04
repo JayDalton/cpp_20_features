8115bb88e1cdc05a5a3d0b43d7c110b49822c8aa
JayDalton <github@t-jay.net>
2022-12-04 17:55:42 +0100

    more concept stuff

--- concept02.h
+++ concept02.h
@@ -28,28 +28,119 @@ namespace Concept02 {
 
    template <typename T>
    concept CFoo = requires(T foo) {
-      { foo.func() } -> std::same_as<int>;
+      // { foo.func() } -> std::same_as<int>;
+      { foo.func() } -> std::integral;
    };
 
    struct Foo {
-      [[nodiscard]] auto func () const -> int { return 42; }
+      [[nodiscard]] auto func () const -> std::int16_t { return 42; }
+   };
+
+   struct Foo1 {
+      [[nodiscard]] auto func () const -> std::int32_t { return 42; }
+   };
+
+   struct Foo2 {
+      [[nodiscard]] auto func () const -> std::int64_t { return 42; }
    };
 
    static_assert(CFoo<Foo>);
+   static_assert(CFoo<Foo1>);
+   static_assert(CFoo<Foo2>);
 
-   auto func(CFoo auto& foo2) {}
+   auto func(CFoo auto& foo2) -> void {}
 
-   template <CFoo TFoo>
+   // template <CFoo TFoo>
+   // class Bar {
+   //    public: 
+   //       constexpr Bar(TFoo input)
+   //       : foo(input) {}
+   //    private:
+   //       TFoo foo{};
+   // };
+
+   template<typename T, typename... Ts>
+   concept same_as_any = (... or std::same_as<T, Ts>);
+
+   template <CFoo... TFoos>
    class Bar {
       public: 
-         constexpr Bar(TFoo input)
+         constexpr Bar(same_as_any<TFoos...> auto input)
          : foo(input) {}
+
+         constexpr auto set_foo(same_as_any<TFoos...> auto input) -> void 
+         {
+            foo = input;
+         }
+
       private:
-         TFoo foo{};
+         std::variant<TFoos...> foo{};
+   };
+
+   template<CFoo... TFoos>
+   class Baz 
+   {
+   public:
+      template<typename T>
+      auto store(T value) -> void {
+         return std::get<std::vector<T>>(data).push_back(value);
+      }
+   private:
+      std::tuple<std::vector<TFoos>...> data{};
    };
 
 
+   template<typename T>
+   concept CDevice = requires(T device) {
+      { T::find_in_env() } -> std::same_as<std::vector<T>>;
+      { device.update() } -> std::same_as<void>;
+   };
 
+   struct Switch {
+      [[nodiscard]] static auto find_in_env() -> std::vector<Switch>
+      {
+         return {{},{},{}};
+      }
+      auto update() -> void { }
+   private:
+      bool is_on{false};   
+   };
+
+   struct Dimmer {
+      [[nodiscard]] static auto find_in_env() -> std::vector<Dimmer>
+      {
+         return {{}, {}};
+      }
+      auto update() -> void { }
+   private:
+      std::uint8_t brightness{0};   
+   };
+
+   template<CDevice... TDevices>
+   struct DeviceManager
+   {
+      DeviceManager() : devices(get_devices()) {}
+
+      auto update() -> void {
+         std::apply(
+            [this](auto &... device_list) {
+               (update_device(device_list), ...);
+            }, devices);
+      }
+   private:
+      using ListType = std::tuple<std::vector<TDevices>...>;
+      auto update_device(auto& device_list) -> void {
+         for (auto& device : device_list) {
+            device.update();
+         }
+      }
+
+      [[nodiscard]] static auto get_devices() -> ListType {
+         return std::tuple{TDevices::find_in_env()...};
+      }
+
+      ListType devices{};
+   };
 
    void call()
    {
@@ -59,5 +150,14 @@ namespace Concept02 {
       Foo foo{};
       func(foo);
 
+      Bar<Foo> bar_1{Foo{}};
+      Bar<Foo1, Foo2> bar_2{Foo1{}};
+
+      Baz<Foo1, Foo2> repo;
+      repo.store(Foo2{});
+      repo.store(Foo1{});
+
+      DeviceManager<Switch, Dimmer> manager{};
+      manager.update(); 
    }
 }
